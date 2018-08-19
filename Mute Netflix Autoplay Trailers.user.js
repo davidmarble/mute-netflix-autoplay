b2f61505cfd94810b8293b505c58e7c7549bbd07
David Marble <davidmarble@gmail.com>
2018-08-19 01:12:16 -0700

    Create Mute Netflix Autoplay Trailers.user.js

---
+++ Mute Netflix Autoplay Trailers.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name Mute Netflix Autoplay Trailers
+// @namespace davidmarble.com
+// @match *://*.netflix.com/*
+// @grant none
+// ==/UserScript==
+
+// Target node for observing mutations
+var target = document.querySelector('.mainView');
+ 
+// Observer instance which will mute any created video elements
+var _node;
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    for (var j=0; j < mutation.addedNodes.length; j++){
+      _node = mutation.addedNodes[j];
+      if (_node.nodeType === 1 && _node.tagName === 'VIDEO'){
+        _node.muted = "muted";
+      }
+    }
+  });
+});
+ 
+// Config: what to listen for
+var config = { 
+  childList: true, 
+  subtree: true
+}
+ 
+// Start observing
+observer.observe(target, config);
